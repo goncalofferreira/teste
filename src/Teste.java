2943b5df9de027dafcaabedaf6ac665cef816576
goncalofferreira <goncalo.fancony@gmail.com>
2018-11-14 20:45:19 +0000

    Initial2

--- src/Teste.java
+++ src/Teste.java
@@ -11,6 +11,6 @@
 public class Teste {
     public static void main(String args[]) 
     { 
-        System.out.println("Isto é um teste");
+        System.out.println("Isto é um testeeee");
     } 
 }
